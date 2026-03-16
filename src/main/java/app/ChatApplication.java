a70be32eeba20db3c011bdfbf6dd3156cf041f0b
freddyjh <fred.jonathan.halden@gmail.com>
2026-03-16 21:56:40 +0100

    Made several changes in .html and java files.
    Signed-off-by: freddyjh <fred.jonathan.halden@gmail.com>

--- src/main/java/app/ChatApplication.java
+++ src/main/java/app/ChatApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("controllermvc")
+@ComponentScan("mvc.controller")
 public class ChatApplication {
 
   public static void main(String[] args) {
