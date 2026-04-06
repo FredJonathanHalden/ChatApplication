9f4b15f915ddb70f7a53ae01e18f703c893b5c94
freddyjh <fred.jonathan.halden@gmail.com>
2026-04-06 17:11:32 +0200

    Made some improvements. Separated Date and Time. Changed Mnr to generate a random value (UUID) instead for a unique id for each added message and security reasons.
    Signed-off-by: freddyjh <fred.jonathan.halden@gmail.com>

--- src/main/java/mvc/model/Message.java
+++ src/main/java/mvc/model/Message.java
@@ -2,61 +2,97 @@ package mvc.model;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
+import java.sql.Date;
+import java.sql.Time;
 
 @Entity
 @Table(name="Message")
 public class Message {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.UUID)
     @Column(name="Mnr", nullable = false, unique = true)
-    private Integer mnr;
+    private byte[] mMnr;
 
     @Column(name="Sdate")
-    private Date sentDate = new Date();
+    private Date mSentDate;
+
+    @Column(name="Stime")
+    private Time mSentTime;
 
     @Column(name="Sname", length=30)
-    private String senderName;
+    private String mSenderName;
 
     @Column(name="Text", length=300)
-    private String text;
+    private String mText;
 
-    public Integer getMnr() {
-        return mnr;
+    public Message() {
+        java.util.Date currentDate = new java.util.Date();
+        mSentDate = new Date(currentDate.getYear(), currentDate.getMonth(), currentDate.getDay());
+        mSentTime = new Time(currentDate.getTime());
+    }
+
+    public byte[] getMnr() {
+        return mMnr;
     }
 
     public Date getSentDate() {
-        return sentDate;
+        return mSentDate;
+    }
+
+    public Time getSentTime() {
+        return mSentTime;
     }
 
     public String getSenderName() {
-        return senderName;
+        return mSenderName;
     }
 
     public String getText() {
-        return text;
+        return mText;
     }
 
-
-    public void setMnr(Integer mMnr) {
-        this.mnr = mMnr;
+    public void setMnr(byte[] mnr) {
+        mMnr = mnr;
     }
 
     public void setSentDate(Date sentDate) {
-        this.sentDate = sentDate;
+        mSentDate = sentDate;
+    }
+
+    public void setSentTime(Time sentTime) {
+        mSentTime = sentTime;
     }
 
     public void setSenderName(String senderName) {
-        this.senderName = senderName;
+        mSenderName = senderName;
     }
 
     public void setText(String text) {
-        this.text = text;
+        mText = text;
     }
 
     @Override
     public String toString() {
-        return "[" + sentDate + "] " + senderName + "> " + text;
+        String timeString = "";
+        long totalSeconds = mSentTime.getTime() / 1000;
+        long seconds = totalSeconds % 60;
+        long minutes = (totalSeconds % 3600) / 60;
+        long hours = (totalSeconds / 3600) - (mSentTime.getTimezoneOffset() / 60);
+        if (hours < 10) {
+            timeString += "0";
+        }
+        timeString += hours + ":";
+
+        if (minutes < 10) {
+            timeString += "0";
+        }
+        timeString += minutes + ":";
+
+        if (seconds < 10) {
+            timeString += "0";
+        }
+        timeString += seconds;
+        return "[" + mSentDate + "][" + timeString + "] " + mSenderName + "> " + mText;
     }
 }
